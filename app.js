7dc4a76c174caf346da3d1816e8e595f90f2f32b
Julek <90630830+Jobrzut@users.noreply.github.com>
2022-08-25 17:38:24 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 console.log('Cześć, jesteś na dobrej drodze by poznać passy 👋 aleee to nie będzie takie łatwe, zaszyfrowałem je, spróbuj się domyślić, podpowiedź to alfabet i brak polskich liter. User to: 8 5 12 12 15; Hasło to: 21 15 17 12 4;');
 
-function check() {
-    var user = document.getElementById('user');
-    var password = document.getElementById('pass');
+const user = document.getElementById('user');
+const password = document.getElementById('pass');
 
-    if (user.value == "hello" && password.value == "world") {
+function check() {
+    if (user.value === "hello" && password.value === "world") {
         location.replace('index2.html');
     } else {
         alert('Niepoprawne Hasło lub Użytkownik');
     }
-    
 }
+
+password.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        check();
+    }
+});
